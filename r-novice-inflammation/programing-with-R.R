c9eddcfadb64f8d1c14fc9228c7e6f25cfcb066c
carloshob <carloshenrique.ob@gmail.com>
2024-06-12 20:48:50 -0300

    finaliza understanding factors

--- r-novice-inflammation/programing-with-R.R
+++ r-novice-inflammation/programing-with-R.R
@@ -683,3 +683,57 @@ write.csv(head(carSpeeds),
 
 
 
+
+
+
+#UNDERSTANDING FACTORS# ----
+
+sex <- factor(c("male", "female", "female", "male"))
+levels(sex)
+nlevels(sex)
+
+food <- factor(c("low", "high", "medium", "high", "low", "medium", "high"))
+levels(food)
+food <- factor(food, levels = c("low", "medium", "high"))
+levels(food)
+
+food <- factor(food, ordered = TRUE)
+levels(food)
+min(food)
+
+
+food <- factor(food, levels = c("low", "medium", "high"), ordered = TRUE)
+levels(food)
+min(food)
+
+exercise <- factor(c("L", "N", "N", "I", "L"), levels = c("N", "L", "I"), ordered = TRUE)
+
+
+f <- factor(c(3.4, 1.2, 5))
+as.numeric(f)
+
+levels(f)[f]
+
+f <- levels(f)[f]
+f <- as.numeric(f)
+
+
+dat <- read.csv(file = 'data/sample.csv', stringsAsFactors = TRUE)
+
+str(dat)
+
+
+summary(dat)
+table(dat$Group)
+barplot(table(dat$Group))
+
+barplot(table(dat$Gender))
+dat$Gender[dat$Gender == 'M'] <- 'm'
+
+
+dat$Gender <- droplevels(dat$Gender)
+plot(x = dat$Gender, y = dat$BloodPressure)
+
+levels(dat$Gender)[2] <- 'f'
+plot(x = dat$Gender, y = dat$BloodPressure)
+
